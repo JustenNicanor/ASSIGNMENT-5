817e703c6666f35967a65c2aa20bf85a6fb1c967
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-17 18:33:46 +0800

    First commit(Adding 'Grade/Mark and 'Description' to print)

--- Assignment 5(1).py
+++ Assignment 5(1).py
@@ -2,44 +2,44 @@ def Grading():
     print("Type '0' If you are Incomplete, Withdrawn or Dropped")
     grade = float(input("Enter your grade: "))
     if grade > 96.4 and grade <= 100:
-       print (1.0)
-       print ("EXCELLENT")
+       print ("Grade/Mark: 1.0")
+       print ("Description: EXCELLENT")
     else:
         if grade > 93.4 and grade <= 96.4:
-           print (1.25)
-           print ("EXCELLENT")
+           print ("Grade/Mark: 1.25")
+           print ("Description: EXCELLENT")
         else:
             if grade > 90.4 and grade <= 93.4:
-               print (1.5)
-               print ("VERY GOOD")
+               print ("Grade/Mark: 1.5")
+               print ("Description: VERY GOOD")
             else:
                 if grade > 87.4 and grade <= 90.4:
-                   print (1.75)
-                   print ("VERY GOOD")
+                   print ("Grade/Mark: 1.75")
+                   print ("Description: VERY GOOD")
                 else:
                     if grade > 84.4 and grade <= 87.4:
-                       print (2.0)
-                       print ("GOOD")
+                       print ("Grade/Mark: 2.0")
+                       print ("Description: GOOD")
                     else:
                         if grade > 81.4 and grade <= 84.4:
-                           print (2.25)
-                           print ("GOOD")
+                           print ("Grade/Mark: 2.25")
+                           print ("Description: GOOD")
                         else:
                             if grade > 78.4 and grade <= 81.4:
-                               print (2.50)
-                               print ("SATISFACTORY")
+                               print ("Grade/Mark: 2.50")
+                               print ("Description: SATISFACTORY")
                             else:
                                 if grade > 75.4 and grade <= 78.4:
-                                   print (2.75)
-                                   print ("SATISFACTORY")
+                                   print ("Grade/Mark: 2.75")
+                                   print ("Description: SATISFACTORY")
                                 else:
                                     if grade > 74.4 and grade <= 75.4:
-                                       print (3.0)
-                                       print ("PASSING")
+                                       print ("Grade/Mark: 3.0")
+                                       print ("Description: PASSING")
                                     else:
                                         if grade <= 74.4 and grade > 64.4:
-                                           print (5.0)
-                                           print ("FAILURE")
+                                           print ("Grade/Mark: 5.0")
+                                           print ("Description: FAILURE")
                                         else:
                                             if grade == 0:
                                                 print ("Pls type 'Inc'(Incomplete), 'W' (Withdrawn), and 'D'(dropped) or")
